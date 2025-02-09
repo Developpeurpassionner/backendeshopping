4d2de7485aaa4f88546d2183bb9335228ae5b565
Yékini ibrahim <97056077+Developpeurpassionner@users.noreply.github.com>
2025-02-09 21:39:04 +0100

    mon premier push

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    // Inscription
+    public function inscription(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'firstname' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => [
+                'required',
+                'string',
+                'min:8',
+                'regex:/[A-Z]/',  // Au moins une majuscule
+                'regex:/[a-z]/',  // Au moins une minuscule
+                'regex:/[0-9]/',  // Au moins un chiffre ,
+            ]
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'Tous les champs doivent respecter les conditions.',
+                'error' => $validator->errors()
+            ], 422);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'firstname' => $request->firstname,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json(['message' => 'Inscription réussie'], 201);
+    }
+
+    // Connexion
+    public function connexion(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            $token = $user->createToken('auth_token')->plainTextToken;
+            return response()->json(['token' => $token], 200);
+        } else {
+        }
+        return response()->json(['error' => 'utilisateur inexistant'], 401);
+    }
+}
