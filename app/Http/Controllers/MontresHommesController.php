c6be762d65827e72ef7a3f05f4fd800883acaf93
Yékini ibrahim <97056077+Developpeurpassionner@users.noreply.github.com>
2025-02-12 00:01:16 +0100

    mon quatrieme commit

---
+++ app/Http/Controllers/MontresHommesController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Montres_Hommes;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class MontresHommesController extends Controller
+{
+    //Création d'une montre pour homme
+    public function CreateMontreHommes(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'nom' => 'required|string|max:255',
+            'photo' => 'required|image|mimes:jpeg,png,jpg,gif,svg',
+            'prix' => 'required|integer',
+            'description' => 'required|string',
+        ]);
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'Tous les champs sont obligatoires et doivent respecter les conditions.',
+                'error' => $validator->errors()
+            ], 422);
+        }
+
+        $montres_hommes = Montres_Hommes::create([
+            'nom' => $request->name,
+            'photo' => $request->photo,
+            'prix' => $request->prix,
+            'description' => $request->description,
+        ]);
+        return response()->json(['message' => 'Montre créé avec sucssès'], 201);
+    }
+
+    // Modification
+    public function UpdateMontreHomme(Request $request, $id)
+    {
+        // Validation des données du formulaire
+        $validator = $request->validate([
+           'nom' => 'required|string|max:255',
+            'photo' => 'required|image|mimes:jpeg,png,jpg,gif,svg',
+            'prix' => 'required|integer',
+            'description' => 'required|string',
+        ]);
+
+        // Trouver la montre par son ID
+        $montres_hommes = Montres_Hommes::findOrFail($id);
+
+        // Mettre à jour la montre avec les nouvelles données
+        $montres_hommes->update($validator);
+
+        // Retourner une réponse JSON avec la montre mise à jour
+        return response()->json(['message' => 'Montre modifiée avec sucssès'], 200);
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'Tous les champs sont obligatoires et doivent respecter les conditions.',
+                'error' => $validator->errors()
+            ], 422);
+        }
+    }
+    // Suppression
+    public function destructionMontreHomme($id)
+    {
+        // Trouver la montre par son ID
+        $montres_hommes = Montres_Hommes::findOrFail($id);
+
+        // Supprimer la montre
+        $montre->delete();
+
+        // Retourner une réponse JSON indiquant la suppression
+        return response()->json(['message' => 'Montre supprimée avec sucssès'], 204);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\MontresHommesController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -12,4 +13,6 @@ Route::post('/inscription', [AuthController::class, 'inscription']); //route pou
 Route::post('/connexion', [AuthController::class, 'connexion']);    //route pour la connexion
 Route::get('auth/{provider}', [SocialAuthController::class, 'redirectToProvider']); //route pour l'inscription avec les réseaux sociaux
 Route::get('auth/{provider}/callback', [SocialAuthController::class, 'handleProviderCallback']); //route pour la connexion avec les réseaux sociaux
-
+Route::post('/montres_pour_hommes', [MontresHommesController::class, 'CreateMontreHommes']); //route pour la création d'une montre pour homme
+Route::put('/montres_pour_hommes/{id}', [MontresHommesController::class, ' UpdateMontreHomme']); //route pour la modification d'une montre pour homme
+Route::delete('/montres_pour_hommes/{id}', [MontresHommesController::class, 'destructionMontreHomme']); //route pour la suppression d'une montre pour homme
